28c5fdf0ff305d5e4384a8c5d2c65b2f0e03b7b3
Evgenia Komleva <komleva.1999@inbox.ru>
2018-10-27 02:48:44 +0300

    3hw

--- src/lib/geolocation.js
+++ src/lib/geolocation.js
@@ -1,24 +1,10 @@
-import {MessageForm, LogInForm} from './lib/message.js';
-import DropZone from './lib/dropzone.js';
-
-import getPosition from './lib/geolocation.js';
-
-
-var gps_info = document.querySelector(".gps-info");
-var gps_button = document.querySelector(".gps-button");
-
-gps_button.addEventListener("submit", function (event) {
-	event.stopPropagation();
-	event.preventDefault();
-  var promise = getPosition();
-	promise.then(showPosition, null);
-})
-
-function showPosition(position) {
-  gps_info.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
+/*function getPosition(options) {
+	return new Promise((resolve, rej) => {
+		window.navigator.geolocation.getCurrentPosition(resolve, rej, options);
+	});
 }
 
+export default getPosition;*/
 
 function geoFindMe() {
     var output = document.getElementById("out");
